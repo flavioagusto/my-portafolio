735d4f06ff7db12546a69287e0a1feb86cf70b67
Flavio <czfayh@gmail.com>
2024-07-23 21:35:23 -0500

    add blog model and view

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    blog_name = models.CharField(max_length=200)
+    blog_content = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.blog_name   
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
+
+def blog(request):
+    blog_data = Blog.objects.all()
+    main_data = {"blog": blog_data}
+    return render(request, "blog.html", main_data)
+
+def home(request):
+    return render(request, "home.html")
+    
